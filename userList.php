75d87eed058d6fe3b438e3fc2c6934ae7d7bead2
geekytechops <142438484+geekytechops@users.noreply.github.com>
2023-12-13 08:11:15 +0530

    Add files via upload

--- userList.php
+++ userList.php
@@ -1,8 +1,23 @@
 <?php 
 
 require_once('config.php');
+$clientIp=$_SERVER['REMOTE_ADDR'];
+$insertIp=$db->execute("INSERT INTO ip_checklist(ip_address)VALUES('$clientIp')");
+if($insertIp){
+    
+    $users=$db->execute("select COUNT(ip_address) as ip_count from ip_checklist WHERE ip_address='$clientIp'");
+    $ipResult=mysqli_fetch_array($users);
+    
+    if($ipResult['ip_count']>3){
+        echo "Request Declined: This IP Address requested the data more than 3 times";
+    }else{
 
-$users=$db->execute('select * from users');
-print_r($_SERVER['REMOTE_ADDR']);
+        
+        
+    }
+
+}
+
+
 
 ?>
