ae52fecea2f57405afb41d96e3c76d70098d5a99
chaitanya <saichaitanya000786@gmail.com>
2024-01-23 12:29:58 +0530

    new2

--- getUsers.php
+++ getUsers.php
@@ -30,7 +30,7 @@ if(isset($_GET['submit'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Get Users List</title>
+    <title>Get Users Lists</title>
 </head>
 <body>
 
